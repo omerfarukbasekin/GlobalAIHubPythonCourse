5d7f195111663736fa201165fac62d564c598f89
omerfarukbasekin <56569869+omerfarukbasekin@users.noreply.github.com>
2021-02-16 22:12:05 +0300

    Add files via upload

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,9 @@
-#Explain your work
-
-#Question 1
-for x in range(a):
-	print(a)
+import random
+a=([1,2,3],[4,5,6],[7,8,9])
+i=0
+for i in range(3):
+    j=0
+    for j in range(3):
+        a[i][j]=random.randint(0,9)
+    
+print(a)
